857d2aaf37af07ba6a7e9e034e8e267d51c65578
Justin Mak <jp.mak44@gmail.com>
2024-10-26 13:07:25 -0400

    adjusted the dropdown so theres no overlap between the navbar and navbar menu. Also removed the border if the hamburger icon is clicked and vice versa

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -13,79 +13,85 @@ const Navbar = () => {
     };
 
     return (
-<nav className="bg-[#FAF6F1]/90 border border-gray flex items-center max-container padding-container fixed top-0 w-full z-50 py-5">
-{/* Logo Section */}
-            <Link href='/'>
-                <Image src="/mantle.png" alt="logo" width={200} height={29} className="ml-4"/>
-            </Link>
+        <nav
+            className={`bg-[#FAF6F1]/90 ${
+                isMobileMenuOpen ? "" : "border border-gray"
+            } flex flex-col max-container padding-container fixed top-0 w-full z-50 py-5`}
+        >
+            {/* Logo Section */}
+            <div className="flex items-center">
+                <Link href='/'>
+                    <Image src="/mantle.png" alt="logo" width={200} height={29} className="ml-4"/>
+                </Link>
 
-            {/* Spacer to push links and buttons to the right */}
-            <div className="flex-grow hidden lg:flex justify-end ">
-                <ul className="flex h-full gap-12 mr-8 mt-2">
-                    {NAV_LINKS.map((link) => (
-                        <li key={link.key} className="flex items-center cursor-pointer transition-all">
-                            <Link href={link.href} className="text-black flex items-center space-x-1 group">
-                                <span className="group-hover:text-[#B3965F] transition-colors">
-                                    {link.label}
-                                </span>
-                                <img
-                                    src="/arrow.svg"
-                                    alt="Arrow Icon"
-                                    className="w-4 h-4 group-hover:text-[#B3965F]"
-                                />
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                
-                {/* Buttons for larger screens */}
-                <div className="space-x-4 mr-6">
-                    <button className="border border-black px-8 py-2 rounded-full text-black hover:bg-gray-100 font-medium">
-                        Login
-                    </button>
-                    <button className="bg-black text-white px-6 py-2 rounded-full hover:bg-gray-800 font-medium">
-                        Get Started
-                    </button>
+                {/* Spacer to push links and buttons to the right */}
+                <div className="flex-grow hidden lg:flex justify-end ">
+                    <ul className="flex h-full gap-12 mr-8 mt-2">
+                        {NAV_LINKS.map((link) => (
+                            <li key={link.key} className="flex items-center cursor-pointer transition-all">
+                                <Link href={link.href} className="text-black flex items-center space-x-1 group">
+                                    <span className="group-hover:text-[#B3965F] transition-colors">
+                                        {link.label}
+                                    </span>
+                                    <img
+                                        src="/arrow.svg"
+                                        alt="Arrow Icon"
+                                        className="w-4 h-4 group-hover:text-[#B3965F]"
+                                    />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    
+                    {/* Buttons for larger screens */}
+                    <div className="space-x-4 mr-6">
+                        <button className="border border-black px-8 py-2 rounded-full text-black hover:bg-gray-100 font-medium">
+                            Login
+                        </button>
+                        <button className="bg-black text-white px-6 py-2 rounded-full hover:bg-gray-800 font-medium">
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+
+                {/* Hamburger Icon for Mobile */}
+                <div className="ml-auto lg:hidden flex items-center mr-4">
+                    <Image
+                        src="/menu.svg"
+                        alt="menu"
+                        width={32}
+                        height={32}
+                        className="cursor-pointer"
+                        onClick={toggleMobileMenu}
+                    />
                 </div>
             </div>
 
-            {/* Hamburger Icon for Mobile */}
-            <div className="ml-auto lg:hidden flex items-center mr-4">
-                <Image
-                    src="/menu.svg"
-                    alt="menu"
-                    width={32}
-                    height={32}
-                    className="cursor-pointer"
-                    onClick={toggleMobileMenu}
-                />
-            </div>
-
             {/* Mobile Menu */}
-{isMobileMenuOpen && (
-    <div className="absolute top-24 left-0 w-full bg-[#FAF6F1] bg-opacity-90 shadow-lg lg:hidden">
-        <ul className="flex flex-col items-center space-y-4 py-4 text-black">
-            {NAV_LINKS.map((link) => (
-                <li key={link.key}>
-                    <Link href={link.href} className="regular-16 text-gray-900 flexCenter cursor-pointer transition-all hover:font-bold">
-                        {link.label}
-                    </Link>
-                </li>
-            ))}
-        </ul>
+            <div>
+                {isMobileMenuOpen && (
+                    <div className="absolute left-0 w-full bg-[#FAF6F1] bg-opacity-90 shadow-lg lg:hidden mt-5">
+                        <ul className="flex flex-col items-center space-y-4 py-4 text-black">
+                            {NAV_LINKS.map((link) => (
+                                <li key={link.key}>
+                                    <Link href={link.href} className="regular-16 text-gray-900 flexCenter cursor-pointer transition-all hover:font-bold">
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
 
-        {/* Login Button in Mobile Menu */}
-        <div className="flex justify-center py-4">
-            <button className="border border-black px-8 py-2 rounded-full text-black hover:bg-gray-100 font-medium">
-                Login
-            </button>
-        </div>
-    </div>
-)}
-
+                        {/* Login Button in Mobile Menu */}
+                        <div className="flex justify-center py-4">
+                            <button className="border border-black px-8 py-2 rounded-full text-black hover:bg-gray-100 font-medium">
+                                Login
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </nav>
     );
 };
 
 export default Navbar;
-
